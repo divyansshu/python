8500460a4066cce02817aba8353dcf90259c83d7
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2024-09-21 14:07:29 +0530

    added comments

--- upes python class assignment/list-1.py
+++ upes python class assignment/list-1.py
@@ -1,33 +1,38 @@
+
+# program to print sum, average, max, min and cummulative sum of the list 
 def operations(lst):
     sum = 0
     list2 = []
+    
     for i in range(len(lst)):
-        sum = sum + lst[i]
-        list2.append(sum)
+        sum = sum + lst[i] # calculating total sum of the list
+        list2.append(sum) #calculating cummulative sum of the list
+        
     print(list2)     
         
-    print(f"sum of list elements is: {sum}")
-    print("average of list elements is : ", sum/len(lst))
+    print(f"sum of list elements is: {sum}") # printing the total sum
+    print("average of list elements is : ", sum/len(lst)) # printing the average of sum
    
     max =  0
     min = lst[0]
-    for i in range(len(lst)):
-        if max < lst[i]:
-            max = lst[i]
-        elif min > lst[i]:
-            min = lst[i]   
-    print("max is: ", max)        
-    print("min is: ", min)        
-         
     
-        
-
+    for i in range(len(lst)):
+        if max < lst[i]: # condition to find maximum element in a list
+            max = lst[i]
+        elif min > lst[i]: # condition to find minimum element in a list
+            min = lst[i]   
+            
+    print("max is: ", max)  # printing maximum      
+    print("min is: ", min)  # printing minimum      
+         
+    # asking from user to enter the elements 
 print("enter the elements of a list")
 
 lst = []
 for i in range(0,5):
     user_input = int(input())
     lst.append(user_input)
-print(lst)
-operations(lst)
+    
+print(lst) # printing the list entered by user
+operations(lst) # calling the method to perform operations on list
 
